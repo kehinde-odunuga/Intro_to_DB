772c8c723304718663890ea8cac215398b9e1f88
kehinde-odunuga <omobolaji92@gmail.com>
2024-10-16 13:09:24 +0100

    A Magical Database for Your Dream Online Bookstore Adventure!

---
+++ alx_book_store.sql
@@ -0,0 +1,46 @@
+-- Create database
+CREATE DATABASE alx_book_store;
+
+-- Use database
+USE alx_book_store;
+
+-- Create Books table
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY, 
+    title VARCHAR(130), author_id INT, 
+    FOREIGN KEY (author_id) REFERENCES authors(id), 
+    price DOUBLE, 
+    publication_date DATE
+);
+
+-- Create Authors table
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY, 
+    author_name VARCHAR(215)
+);
+
+-- Create Customers table
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY, 
+    customer_name VARCHAR(215), 
+    email VARCHAR(215), 
+    [address] TEXT
+);
+
+-- Create Orders table
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY, 
+    customer_id INT, 
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), 
+    Order_date DATE
+);
+
+-- Create Order_Details table
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY, 
+    order_id INT, 
+    FOREIGN KEY (order_id) REFERENCES orders(order_id), 
+    book_id INT, 
+    FOREIGN KEY (book_id) REFERENCES books(book_id), 
+    quantity DOUBLE
+);
